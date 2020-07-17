4f681c34e1b4b04dfbe0c44e86731d54717d5392
Chayce SOlchaga <chayce.solchaga@gmail.com>
2020-07-17 10:27:23 -0700

    everything working but the data again

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,31 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import Character from './Character';
 import axios from 'axios';
-import { BASE_URL, CHARACTER_KEY } from './components/constants';
+import { BASE_URL, CHARACTER_KEY } from './constants';
 import './App.css';
-import Details from './components/Details'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 const [character, setCharacter] = useState([])
 const [currentCharacterId, setCurrentCharacterId] = useState('1')
-  
+
 const openDetails = id => {
-  setCharacter(id)
+  setCurrentCharacterId(id)
 }
 
 const closeDetails = () => {
   setCurrentCharacterId(null)
 }
-
-
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 useEffect(() => {
   axios.get(`${BASE_URL}${CHARACTER_KEY}`)
   .then( res => {
-    setCharacter(res.results.data)
+    setCharacter(res.data.results)
     console.log(res)
   })
   .catch(err => {
@@ -33,23 +31,24 @@ useEffect(() => {
   })
 }, []);
 
-const Character = ({ info }) => (
-  <div className='characterList'>
+const CharacterList = ({ info }) => (
+  <div className='character'>
     {info.name}
     <button onClick={() => openDetails(info.id)}>
-      See Details</button>
+      See details
+    </button>
   </div>
 )
   return (
     <div className="App">
-      <h1 className="Header">Rick and Morty!</h1>
+      <h1 className="Header">Rick and Morty Characters!</h1>
       {
         character.map(person => {
-          return <Character key={person.id} info={person} />
+          return <CharacterList key={person.id} info={person} />
         })
       }
       {
-        currentCharacterId && <Details alert={currentCharacterId} characterId={currentCharacterId} close={closeDetails} />
+        currentCharacterId && <Character alert={currentCharacterId} keyId={currentCharacterId} close={closeDetails} />
       }
     </div>
   );
