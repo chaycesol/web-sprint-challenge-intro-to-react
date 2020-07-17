55784100be2d5e502b2206fa7e1ecb8ea66a4f07
Chayce SOlchaga <chayce.solchaga@gmail.com>
2020-07-17 08:28:13 -0700

    connected to rick and morty api

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BASE_URL, CHARACTER_KEY, LOCATION_KEY, EPISODE_KEY } from './components/constants';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const [character, setCharacter] = useState({})
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+useEffect(() => {
+  axios.get(`${BASE_URL}${CHARACTER_KEY}`)
+  .then( res => {
+    setCharacter(res.data)
+    console.log(res)
+  })
+  .catch(err => {
+    console.log('Character API Error' + err)
+  })
+}, []);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
---
+++ starwars/src/components/constants/index.js
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://rickandmortyapi.com/api'
+export const CHARACTER_KEY= '/character/'
+export const LOCATION_KEY = '/location/'
+export const EPISODE_KEY = '/episode/'
